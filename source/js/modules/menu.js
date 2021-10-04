314d018dcb0b46d2c49c77ed37bdfa3cafbe84ee
v.trubicin <v.trubicin@unitcode.ru>
2021-10-04 18:13:36 +0300

    module1-task3

--- source/js/modules/menu.js
+++ source/js/modules/menu.js
@@ -15,8 +15,18 @@ export default () => {
     });
   }
 
+  const overlay = document.querySelector('.overlay');
+
   for (let i = 0; i < menuLinks.length; i++) {
-    menuLinks[i].addEventListener(`click`, function () {
+    menuLinks[i].addEventListener(`click`, function (evt) {
+      if (i === 2) {
+        evt.preventDefault();
+        overlay.classList.add('active');
+        setTimeout(() => {
+          window.location.hash = '#prizes';
+          overlay.classList.remove('active');
+        }, 350);
+      }
       if (window.innerWidth < 1025) {
         header.classList.remove(`page-header--menu-opened`);
         document.body.classList.remove(`menu-opened`);
